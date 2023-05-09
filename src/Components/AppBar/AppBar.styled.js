d75d39e12269f065f79d0ff8708a68dca2df0967
IHOR <lavrenchyk.igor@gmail.com>
2023-05-09 23:27:13 +0300

    34

--- src/Components/AppBar/AppBar.styled.js
+++ src/Components/AppBar/AppBar.styled.js
@@ -1,18 +1,15 @@
-import { NavLink } from "react-router-dom";
-import styled from "styled-components";
-
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 
 export const Header = styled.header`
-    display: flex;
-    gap: 60px;
-    padding: 20px;
-    align-items: center;
-    background-color: #EBD8FF;
-
+  display: flex;
+  gap: 60px;
+  padding: 20px;
+  align-items: center;
+  background-color: #0abab5;
 `;
 
 export const Logo = styled(NavLink)`
-    text-decoration: none;
-    color: #00212f;
+  text-decoration: none;
+  color: #00212f;
 `;
-
